ad5dc8641f7183975e7406e423d0575bfb197269
kolesnykt25 <88713291+kolesnykt25@users.noreply.github.com>
2021-12-10 11:44:55 +0200

    1

--- WebConfiguration/GameViewController.swift
+++ WebConfiguration/GameViewController.swift
@@ -11,7 +11,6 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
 }
---
+++ WebConfiguration/WebViewController.swift
@@ -0,0 +1,33 @@
+//
+//  WebViewController.swift
+//  WebConfiguration
+//
+//  Created by Taras Kolesnyk on 09.12.2021.
+//
+
+import UIKit
+import WebKit
+// http://apps.vortexads.io/guest?app=666666&uuid=123
+
+class WebViewController: UIViewController {
+    
+    var webView: WKWebView!
+    
+    var url: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if (url != nil)
+        {
+            loadView(url: url)
+        }
+}
+    
+    func loadView(url: String) {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        view = webView
+        webView.load(URLRequest(url: URL(string: url)!))
+    }
+
+}
